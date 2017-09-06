f3b33f07b191e197beb7f69be6a163b603b9d925
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-09-06 23:02:49 +0000

    Move old-docs out of master

--- doc/js/search_index.js
+++
@@ -1,1 +0,0 @@
-var search_data = {"index":{"searchIndex":["36"],"longSearchIndex":[""],"info":[["36","","__/__/__/__/__/__/tmp/vaV0Sur/36.html","","<p>check                 package:devtools                 R Documentation\n<p>Build and check a package, cleaning …\n"]]}}
